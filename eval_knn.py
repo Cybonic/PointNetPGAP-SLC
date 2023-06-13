fc2e9dc8fe9ebea485acf555d810c22ab2cb0cc8
tiago <tiago.barros.silva@hotmail.com>
2023-06-13 01:03:02 +0100

    save data

--- eval_knn.py
+++ eval_knn.py
@@ -150,29 +150,60 @@ class PlaceRecognition():
         #for line in self.true_loop_bool:
         #    self.true_loop[line==true] 
 
+        # SAVE PREDICTIONS
+        self.predictions_dir = os.path.join('saved_model_data',f'{str(self.model)}',f'{self.dataset_name}')
+        #self.prediction_file = os.path.join(predictions_dir,f'{str(self.model)}')
+        if not os.path.isdir(self.predictions_dir):
+            os.makedirs(self.predictions_dir)
+            logger.warning('\n ** Created a new directory: ' + self.predictions_dir)
+
         # SAVE DESCRIPTORS
-        self.descriptors_dir = os.path.join('predictions',f'{self.dataset_name}','descriptors')
-        self.descriptor_file = os.path.join(self.descriptors_dir,f'{str(self.model)}.torch') 
-        if self.save_deptrs==True and not os.path.isdir(self.descriptors_dir):
-            os.makedirs(self.descriptors_dir)
-            logger.warning('\n ** Created a new directory: ' + self.descriptors_dir)
+        #self.descriptors_dir = os.path.join('predictions',f'{self.dataset_name}','descriptors')
+        #self.descriptor_dir = os.path.join(self.predictions_dir,'descriptors') 
+        #if not os.path.isdir(self.descriptor_dir):
+        #    os.makedirs(self.descriptor_dir)
+        #    logger.warning('\n ** Created a new directory: ' + self.descriptor_dir)
         
         # SAVE RESULTS
-        results_dir = os.path.join('predictions',f'{self.dataset_name}','place','results')
-        self.results_file = os.path.join(results_dir,f'{str(self.model)}')
-        if not os.path.isdir(results_dir):
-            os.makedirs(results_dir)
-            logger.warning('\n ** Created a new directory: ' + results_dir)
+        #self.results_dir = os.path.join(self.predictions_dir,'place','results')
+        #if not os.path.isdir(self.results_dir):
+        #    os.makedirs(self.results_dir)
+        #    logger.warning('\n ** Created a new directory: ' + self.results_dir)
         
-        # SAVE PREDICTIONS
-        predictions_dir = os.path.join('predictions',f'{self.dataset_name}','place','loops')
-        self.prediction_file = os.path.join(predictions_dir,f'{str(self.model)}')
-        if not os.path.isdir(predictions_dir):
-            os.makedirs(predictions_dir)
-            logger.warning('\n ** Created a new directory: ' + predictions_dir)
+        # SAVE RESULTS
+        #self.pred_dir = os.path.join(self.predictions_dir,'place','predictions')
+        #if not os.path.isdir(self.pred_dir):
+        #    os.makedirs(self.pred_dir)
+        #    logger.warning('\n ** Created a new directory: ' + self.pred_dir)
         
+        
+        
+    def load_descriptors(self,file):
 
-    def load_descriptors(self,file=None):
+        
+        target_dir = os.path.join(self.predictions_dir,file)
+        if not os.path.isdir(target_dir):
+            os.makedirs(target_dir)
+        
+        file = os.path.join(target_dir,'descriptors.torch')
+            
+        if not os.path.isfile(file): 
+            self.logger.error("\n ** File does not exist: "+ file)
+            self.logger.warning("\n ** Generating descriptors!")
+            return
+        
+        else:
+            self.logger.warning('\n ** Loading descriptors from internal File: ' + file)
+
+        # LOADING DESCRIPTORS
+        self.descriptors = torch.load(file)
+
+        self.use_load_deptrs = True # Disable descriptor generation
+        self.save_deptrs = False # Descriptors were already saved, so no need to save again
+    
+
+
+    def save_descriptors(self,file=None):
         '''
         Load previously generated descriptors.
 
@@ -181,20 +212,20 @@ class PlaceRecognition():
            which is based on the model and dataset name  
         '''
         if file == None:
-            file = self.descriptor_file # Internal File name 
-            
-        if not os.path.isfile(file): 
-            self.logger.error("\n ** File does not exist: "+ file)
-            self.logger.warning("\n ** Generating descriptors!")
-            return
-        
+            target_dir = os.path.join(self.predictions_dir,self.score_value)
         else:
-            self.logger.warning('\n ** Loading descriptors from internal File: ' + self.descriptor_file)
+            target_dir = os.path.join(self.predictions_dir,file)
+        
+        # Create directory
+        if not os.path.isdir(target_dir):
+            os.makedirs(target_dir)
+        file = os.path.join(target_dir,'descriptors.torch')
 
         # LOADING DESCRIPTORS
-        self.descriptors = torch.load(file)
-        self.use_load_deptrs = True # Disable descriptor generation
-        self.save_deptrs = False # Descriptors were already saved, so no need to save again
+        if self.save_deptrs == True:
+            torch.save(self.descriptors,file)
+            self.logger.warning('\n ** Saving descriptors at File: ' + file)
+ 
 
 
     def get_descriptors(self):
@@ -207,23 +238,30 @@ class PlaceRecognition():
     def save_predictions_cv(self,file=None):
         # SAVE PREDICTIONS
         # Check if the predictions were generated
-        assert  hasattr(self, 'pred_loops') and \
-                hasattr(self, 'pred_scores') and   \
-                hasattr(self, 'target_loops'), 'Predictions were not generated!'
+        assert  hasattr(self, 'predictions')
         
+        loop_cand   = self.predictions['loop_cand']
+        loop_scores = self.predictions['loop_cand']
+        gt_loops    = self.predictions['loop_cand']
+
         assert hasattr(self, 'score_value'), 'Results were not generated!'
         
         if file == None:
-            file = self.prediction_file # Internal File name 
-            self.logger.warning('\n ** Saving results from internal File: ' + self.prediction_file)
+            target_dir = os.path.join(self.predictions_dir,self.score_value,'place') # Internal File name 
+        else:
+            target_dir = os.path.join(self.predictions_dir,file,'place') # Internal File name 
+        
+        if not os.path.isdir(target_dir):
+            os.makedirs(target_dir)
+        self.logger.warning('\n ** Saving predictions at: ' +target_dir)
 
-        df_pred   = pd.DataFrame(self.pred_loops)
-        df_score  = pd.DataFrame(self.pred_scores)
-        df_target = pd.DataFrame(self.target_loops)
+        df_pred   = pd.DataFrame(loop_cand)
+        df_score  = pd.DataFrame(loop_scores)
+        df_target = pd.DataFrame(gt_loops)
     
-        file_results_ped = self.prediction_file + f'_loops_{self.score_value}.csv'
-        file_results_score =self.prediction_file + f'_scores_{self.score_value}.csv'
-        file_target =self.prediction_file + f'_target_{self.score_value}.csv'
+        file_results_ped = os.path.join(target_dir,f'loops.csv')
+        file_results_score =os.path.join(target_dir,f'scores.csv')
+        file_target =os.path.join(target_dir,f'target.csv')
 
         df_pred.to_csv(file_results_ped)
         df_score.to_csv(file_results_score)
@@ -236,8 +274,14 @@ class PlaceRecognition():
         # Check if the results were generated
         assert hasattr(self, 'results'), 'Results were not generated!'
         if file == None:
-            file = self.results_file # Internal File name 
-            logger.warning('\n ** Saving results from internal File: ' + self.results_file)
+            target_dir = os.path.join(self.predictions_dir,self.score_value,'place') # Internal File name 
+        else:
+            target_dir = os.path.join(self.predictions_dir,file,'place') # Internal File name 
+        
+        if not os.path.isdir(target_dir):
+            os.makedirs(target_dir)
+        
+        logger.warning('\n ** Saving results from internal File: ' + target_dir)
 
         top_cand = np.array(list(self.results.keys())).reshape(-1,1)
         colum = ['top']
@@ -251,7 +295,7 @@ class PlaceRecognition():
         rows = np.array(rows)
         rows = np.concatenate((top_cand,rows),axis=1)
         df = pd.DataFrame(rows,columns = colum)
-        file_results = file + '_' + self.score_value +'.csv'
+        file_results = os.path.join(target_dir,'results.csv')
         df.to_csv(file_results)
         self.logger.warning("Saved results at: " + file_results)
 
@@ -265,10 +309,6 @@ class PlaceRecognition():
         if self.use_load_deptrs == False:
             self.descriptors = self.generate_descriptors(self.model,self.loader)
         
-        # SAVE DESCRIPTORS
-        if self.save_deptrs:
-            torch.save(self.descriptors,self.descriptor_file)
-            self.logger.info("Descriptors saved at: " + self.descriptor_file)
 
         # COMPUTE TOP 1%
         # Compute number of samples to retrieve correspondin to 1% 
@@ -279,10 +319,9 @@ class PlaceRecognition():
         # COMPUTE RETRIEVAL
         # Depending on the dataset, the way datasets are split, different retrieval approaches are needed. 
         # the kitti dataset 
-        metric,predictions = eval_place(self.anchors,self.descriptors,self.poses,max_top,window=self.window)
+        metric,self.predictions = eval_place(self.anchors,self.descriptors,self.poses,max_top,window=self.window)
 
-        # SAVE PREDICTIONS
-
+ 
         # RE-MAP TO AN OLD FORMAT
         remapped_old_format={}
         for top in tqdm(range(1,max_top),'Performance'):
@@ -310,6 +349,8 @@ class PlaceRecognition():
                 # Generate the Descriptor
                 prediction,feat = model(input)
                 assert prediction.isnan().any() == False
+                if len(prediction.shape)<2:
+                    prediction = prediction.unsqueeze(0)
                 # Keep descriptors
                 for d,i in zip(prediction.detach().cpu().numpy().tolist(),inx.detach().cpu().numpy().tolist()):
                     prediction_bag[int(i)] = d
@@ -325,7 +366,7 @@ if __name__ == '__main__':
         '--network', '-m',
         type=str,
         required=False,
-        default='scancontext',
+        default='ORCHNet',
         help='Directory to get the trained model.'
     )
     
@@ -449,13 +490,14 @@ if __name__ == '__main__':
     ###################################################################### 
 
     # DATALOADER
+    SESSION['max_points']= FLAGS.max_points
+    SESSION['retrieval']['top_cand'] = list(range(1,25,1))
+
     model_,dataloader = pipeline(FLAGS.network,FLAGS.dataset,SESSION)
     model_wrapper = contrastive.ModelWrapper(model_,**SESSION['modelwrapper'])
     #dataloader = utils.load_dataset(FLAGS.dataset,SESSION,FLAGS.memory)                            
     
-    SESSION['train_loader']['data']['max_points'] = FLAGS.max_points
-    SESSION['val_loader']['data']['max_points'] = FLAGS.max_points
-    SESSION['retrieval']['top_cand'] = list(range(1,25,1))
+    
 
     logger = logging.getLogger("Knn Eval")
 
@@ -465,16 +507,16 @@ if __name__ == '__main__':
                         600, # Warmup
                         'L2', # Similarity Metric
                         logger, # Logger
-                        save_deptrs = True,
                         device  = FLAGS.device # Device
                         ) # ,windows,eval_metric,device
     
     # Load the descriptors if they exist
-    pl.load_descriptors()
+    pl.load_descriptors('temp')
     # Run place recognition evaluation
     pl.run()
-    pl.save_predictions_cv()
-    pl.save_results_cv()
+    pl.save_descriptors('temp')
+    pl.save_predictions_cv('temp')
+    pl.save_results_cv('temp')
 
     
 
