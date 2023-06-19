ac95832ce215bc89c785a56b90971f47c91ffdbc
Tiago Barros <tiagobarros@MacBook-Pro-de-Tiago.local>
2023-06-19 17:32:21 +0200

    added main

--- pipeline_factory.py
+++ pipeline_factory.py
@@ -8,13 +8,13 @@ from dataloader.laserscan import Scan
 
 def pipeline(network,dataset,session):
 
-
     if network == 'LOGG3D':
         # get nework
         num_points=session['max_points']
         output_dim=256
-        modality = SparseLaserScan(max_points=num_points,
+        modality = SparseLaserScan(voxel_size=0.05,max_points=num_points,
                                    aug_flag=session['aug'])
+        
     elif network in ['PointNetVLAD','ORCHNet']:
         num_points = session['max_points']
         output_dim=256
@@ -23,6 +23,19 @@ def pipeline(network,dataset,session):
 
     model_ = get_pipeline(network,num_points=num_points)
 
-    loader = utils.load_dataset(dataset,session,modality)
+    loader = utils.make_data_loader(dataset,session,modality)
 
     return model_,loader
+
+
+if __name__=="__main__":
+    import yaml,os
+    
+    dataset = 'kitti'
+    session_cfg_file = os.path.join('sessions', dataset.lower() + '.yaml')
+    SESSION = yaml.safe_load(open(session_cfg_file, 'r'))
+    Model,dataloader = pipeline('LOGG3D','kitti',SESSION)
+    print(Model)
+    print(dataloader)
+    
+    #assert str(Model)=="LO"
