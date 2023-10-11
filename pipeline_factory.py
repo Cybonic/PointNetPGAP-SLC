bb17384a2dfc5213d3e8e7987096bf691f5c192d
tiago <you@example.com>
2023-10-11 13:10:25 +0100

    refactory

--- pipeline_factory.py
+++ pipeline_factory.py
@@ -6,7 +6,37 @@ from dataloader.projections import BEVProjection,SphericalProjection
 from dataloader.sparselaserscan import SparseLaserScan
 from dataloader.laserscan import Scan
 
-def pipeline(network,dataset,session):
+
+
+MODELS = ['LOGG3D','PointNetVLAD','ORCHNet','overlap_transformer']
+
+def model_handler(pipeline_name, num_points=4096,output_dim=256):
+    
+    from networks.pipelines.PointNetVLAD import PointNetVLAD
+    from networks.pipelines.LOGG3D import LOGG3D
+    from networks.pipelines.ORCHNet import ORCHNet
+    from networks.pipelines.overlap_transformer import featureExtracter
+
+    print("\n**************************************************")
+    print(f"Model: {pipeline_name}")
+    print(f"N.points: {num_points}")
+    print(f"Dpts: {output_dim}")
+    print("**************************************************\n")
+
+    assert pipeline_name in MODELS
+    if pipeline_name == 'LOGG3D':
+        pipeline = LOGG3D(output_dim=output_dim)
+    elif pipeline_name == 'PointNetVLAD':
+        pipeline = PointNetVLAD( use_tnet=True, output_dim=output_dim, num_points = num_points,feat_dim = 1024)
+    elif pipeline_name == 'ORCHNet':
+        pipeline = ORCHNet('pointnet',output_dim=output_dim, num_points=num_points,feat_dim = 1024)
+    elif pipeline_name == 'overlap_transformer':
+        pipeline = featureExtracter(channels=1,height=64, width=900, output_dim=output_dim, use_transformer = True,
+                                    feature_size=1024, max_samples=num_points)    
+    return pipeline
+
+
+def dataloader_handler(root_dir,network,dataset,session):
 
     if session['modality'] == "bev":
         modality = BEVProjection(2256,2256)
@@ -14,23 +44,37 @@ def pipeline(network,dataset,session):
         modality = SphericalProjection(2256,2256)
 
     if network == 'LOGG3D':
-        # get nework
+        # Get sparse (voxelized) point cloud based modality
         num_points=session['max_points']
         output_dim=256
         modality = SparseLaserScan(voxel_size=0.05,max_points=num_points,
                                    aug_flag=session['aug'])
-        
+    
     elif network in ['PointNetVLAD','ORCHNet']:
+        # Get point cloud based modality
         num_points = session['max_points']
         output_dim=256
         modality = Scan(max_points=num_points,
                         aug_flag=session['aug'])
 
-    model_ = get_pipeline(network,num_points=num_points)
+    #loader = utils.make_data_loader(root_dir,dataset,session,modality)
 
-    loader = utils.make_data_loader(dataset,session,modality)
+    dataset = dataset.lower()
+    assert dataset in ['kitti','orchard-uk','uk','pointnetvlad'],'Dataset Name does not exist!'
 
-    return model_,loader
+    from dataloader.kitti.kitti import KITTI as DATALOADER
+    
+    loader = DATALOADER( root = root_dir,
+                    dataset = dataset,
+                    modality = modality,
+                    memory   = session['memory'],
+                    train_loader  = session['train_loader'],
+                    val_loader    = session['val_loader'],
+                    max_points    = session['max_points']
+                    )
+
+    return loader
+
 
 
 if __name__=="__main__":
