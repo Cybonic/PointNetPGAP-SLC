584bb416640d331ef8abec02f983c3c72143b79c
tiago <tiago.barros.silva@hotmail.com>
2024-04-03 18:06:36 +0100

    added stages

--- script_train.py
+++ script_train.py
@@ -17,21 +17,24 @@ density = '10000'
 
 input_preprocessing = ' --roi 0 --augmentation 1 --shuffle_points 1'
 
-test_sequences = ['OJ22','OJ23','ON22', 'SJ23']
+test_sequences = ['ON22']#'OJ22','OJ23','ON22', 'SJ23']
 
-for seq in test_sequences:
-        func_arg = [
-                '--train 0', # Train or test
-                f'--dataset_root {dataset_root}', # path to Dataset 
-                '--resume best_model', # [best_model, last_model]
-                f'--val_set {seq}',
-                '--memory RAM', # [DISK, RAM] 
-                '--device cuda', # Device
-                f'--save_predictions {save_path}', # Save predictions
-                f'--epochs {epochs}',
-                input_preprocessing
-        ]
+stages = ['011','101','001']
+for stage_conf in stages:
+        for seq in test_sequences:
+                func_arg = [
+                        '--train 0', # Train or test
+                        f'--dataset_root {dataset_root}', # path to Dataset 
+                        '--resume best_model', # [best_model, last_model]
+                        f'--val_set {seq}',
+                        '--memory RAM', # [DISK, RAM] 
+                        '--device cuda', # Device
+                        f'--save_predictions {save_path}', # Save predictions
+                        f'--epochs {epochs}',
+                        f'--stages {stage_conf}',
+                        input_preprocessing
+                ]
+                        
+                func_arg_str = ' '.join(func_arg)        
                 
-        func_arg_str = ' '.join(func_arg)        
-        
-        os.system('python3 train_knn.py ' + func_arg_str)
+                os.system('python3 train_knn.py ' + func_arg_str)
