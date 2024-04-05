17cc749a7da04463b028bc6ff08766a15834238d
tiago <tiago.barros.silva@hotmail.com>
2024-04-05 18:28:34 +0100

    updated to PointNetKeypoint

--- train_knn.py
+++ train_knn.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         '--network', '-m',
         type=str,
         required=False,
-        default='PointNetGAPLoss',
+        default='PointNetKeypointLoss',
         help='Directory to get the trained model.'
     )
 
