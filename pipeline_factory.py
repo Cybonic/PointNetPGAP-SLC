e0225c0ef66c59718840dc9a8542a92dd0368083
tiago <tiago.barros.silva@hotmail.com>
2024-04-05 18:35:33 +0100

    update

--- pipeline_factory.py
+++ pipeline_factory.py
@@ -83,7 +83,7 @@ def model_handler(pipeline_name, num_points=4096,output_dim=256,feat_dim=1024,de
 
     if pipeline_name in ['LOGG3D'] or pipeline_name.startswith("SPV"):
         model = contrastive.SparseModelWrapper(pipeline,loss = loss,device = device,**argv['trainer'])
-    elif pipeline_name in ['PointNetGAPLoss','PointNetHGAPLoss']:
+    elif pipeline_name.endswith('Loss'):
         model = contrastive.ModelWrapperLoss(pipeline,
                                              loss = loss,
                                              aux_loss = 'segment_loss', # 'segment_loss', # ['segment_loss',None]
