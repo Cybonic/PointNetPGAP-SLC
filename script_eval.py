b85cd9409f08f8c6a738cfc059d731218dca1247
tiago <tiago.barros.silva@hotmail.com>
2024-04-09 23:45:39 +0100

    update

--- script_eval.py
+++ script_eval.py
@@ -16,13 +16,14 @@ test_sequences = ['OJ22','OJ23','ON22','SJ23']
 
 resume_root = "/home/tiago/workspace/pointnetgap-RAL/RALv2/predictions_RALv1"
 for seq in test_sequences:
-        for network in ['LOGG3D','overlap_transformer','PointNetGAP','PointNetGeM','PointNetMAC','PointNetVLAD']:
+        for network in ['overlap_transformer','PointNetGAP','PointNetGeM','PointNetMAC','PointNetVLAD']:
                 func_arg = [ 
                         f'--dataset_root {dataset_root}', # path to Dataset 
                         f'--val_set {seq}',
                         f'--resume {resume_root}/{network}/{seq}/descriptors.torch',
                         '--memory DISK', # [DISK, RAM] 
                         '--device cuda', # Device
+                        f'--network {network}', # Network
                         #f'--save_predictions {save_path}', # Save predictions
                         ]
                 
