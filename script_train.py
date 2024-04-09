9e78b53bc536d52afbb67956772846737c88cc2d
tiago <tiago.barros.silva@hotmail.com>
2024-04-09 03:03:27 +0100

    update

--- script_train.py
+++ script_train.py
@@ -17,7 +17,7 @@ density = '10000'
 
 input_preprocessing = ' --roi 0 --augmentation 1 --shuffle_points 1'
 
-test_sequences = ['ON22']#,'OJ22','OJ23','ON22', 'SJ23']
+test_sequences = ['ON22','OJ22','OJ23','ON22', 'SJ23']
 
 stages = ['001']
 for stage_conf in stages:
