f170b3bdb1d79b8a5574a213198f489d1f3b43ed
tiago <tiago.barros.silva@hotmail.com>
2024-04-10 11:14:31 +0100

    cleaning

--- script_eval.py
+++ script_eval.py
@@ -9,9 +9,6 @@ dataset_root = '/home/tiago/workspace/DATASET'
 
 resume  = "PointNetGAP.pth" # choice [checkpoints.pth, descriptors.torch]
 
-# Path to save the predictions
-save_path  = 'predictions'
-
 test_sequences = ['OJ22','OJ23','ON22','SJ23']
 
 resume_root = "/home/tiago/workspace/pointnetgap-RAL/RALv2/predictions_RALv1"
@@ -24,7 +21,6 @@ for seq in test_sequences:
                         '--memory DISK', # [DISK, RAM] 
                         '--device cuda', # Device
                         f'--network {network}', # Network
-                        #f'--save_predictions {save_path}', # Save predictions
                         ]
                 
                 func_arg_str = ' '.join(func_arg)
