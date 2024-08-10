8aada5b8bfbc18dc7c35e486d32e7a5f491985b2
Tiago Barros <tiagobarros@isr.uc.pt>
2024-08-10 14:51:58 +0100

    adaptation for cross domain

--- script_eval.py
+++ script_eval.py
@@ -4,25 +4,28 @@ import os
 # Define the path to the dataset
 dataset_root = '/home/tbarros/workspace/DATASET'
 
-test_sequences = ['SJ23',
-                  'ON22',
-                  'OJ22',
-                  'ON23',
-                  'OJ23',
-                  'GTJ23'
+test_sequences = ['00',
+                  '02',
+                  '05',
+                  '06',
+                  '08',
+                  #'GTJ23'
                 ]
 
-resume_root = "/home/tbarros/workspace/pointnetgap-RAL/RALv3/predictions"
+windows = [600,600,600,600,600,600]
+resume_root = "/home/tbarros/workspace/pointnetgap-RAL/RALv3/kitti_predictions"
 
-for seq in test_sequences:
+for seq,win in zip(test_sequences,windows):
         for network in ['PointNetPGAP']:
                 func_arg = [ 
                         f'--dataset_root {dataset_root}', # path to Dataset 
+                        f'--dataset HORTO-3DLM',
                         f'--val_set {seq}',
-                        f'--resume {resume_root}/#{network}-LazyTripletLoss_L2/eval-{seq}/descriptors.torch',
+                        f'--resume {resume_root}/#{network}/eval-{seq}/descriptors.torch',
                         '--memory DISK', # [DISK, RAM] 
                         '--device cuda', # Device
                         f'--network {network}', # Network
+                        f'--eval_roi_window {win}',
                         ]
                 
                 func_arg_str = ' '.join(func_arg)
